f102be4cf29ee6aaaa1b62577951b0c5527e5aed
Manojpatel-coder <124519634+Manojpatel-coder@users.noreply.github.com>
2025-05-05 01:46:42 +0530

    Update listing.js

--- controller/listing.js
+++ controller/listing.js
@@ -1,4 +1,4 @@
-const Listing=require('../model/Listing.js');
+const Listing=require('../model/listing.js');
 const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 const mapToken=process.env.MAP_TOKEN;
 const geocodingClient=mbxGeocoding({accessToken:mapToken});
